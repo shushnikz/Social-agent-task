af6a9afc8db64dfb5ed1d474ce69abf83a148e88
shushnikz <sushmithadevadiga997@gmail.com>
2023-05-05 19:00:17 +0530

    added carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -62,7 +62,14 @@ function Home() {
                         </div>
                     </div>
                 </div>
-
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
     )
